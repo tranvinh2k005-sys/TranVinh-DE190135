a884fd3e506fbefb50909dc3e0b7003958460db9
Trần Vinh <tranvinh@gmail.com>
2026-09-14 08:33:10 +0700

    Add About component and props

--- SL3/src/App.js
+++ SL3/src/App.js
@@ -1,7 +1,24 @@
+import './App.css'
+import About from './About'
+
 function App() {
+  // khai báo 1 object user
+  const user1 = {
+    name: "TrầnLTB",
+    email: "traltb@fe.edu.vn"
+  }
+
+  const user2 = {
+    name: "Ngoc",
+    email: "ngoc@gmail.com"
+  }
+
   return (
-    <h1>Hello World</h1>
-  );
+    <>
+      <About user={user1} />
+      <About user={user2} />
+    </>
+  )
 }
 
-export default App;
+export default App
